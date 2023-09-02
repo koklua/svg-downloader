f2c5d7a362e377facd6a1bc728d342e4d419947b
yoktanela <elanuryoktan@gmail.com>
2023-09-02 22:59:45 +0300

    Add a function to get rgb values from rgba string

--- background.js
+++ background.js
@@ -15,11 +15,19 @@ function weightedColor(svgElement) {
 
     var midValue = 0;
     for (const color of colors) {
-        // TODO: Check if it is hex or rgba value
-        var value = color.match(/#[0-9a-fA-F]{3,6}|rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)/g);
-        var rgb = hexToRgb(value);
-        // TODO: Add a weight based on the area of current path.
+      // TODO: Add a weight based on the area of current path.
+      var hexValue = color.match(/#[0-9a-fA-F]{3,6}/g);
+
+      if (hexValue != null) {
+        var rgb = hexToRgb(hexValue);
         midValue += (rgb.r + rgb.g + rgb.b)/3;
+      } else {
+        var rgbaValue = color.match(/rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)/g);
+        if (rgbaValue != null) {
+          let rgb = rgbaToRgb(rgbaValue);
+          midValue += (rgb.r + rgb.g + rgb.b)/3;
+        }
+      }
     }
 
     midValue /= colors.length;
@@ -37,4 +45,13 @@ function hexToRgb(hex) {
         g: parseInt(result[2], 16),
         b: parseInt(result[3], 16)
     } : null;
+}
+
+function rgbaToRgb(rgba) {
+  var result = /^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/i.exec(rgba);
+  return result ? {
+    r: parseInt(result[1]),
+    g: parseInt(result[2]),
+    b: parseInt(result[3])
+  } : null;
 }
